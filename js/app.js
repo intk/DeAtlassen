c46b3cfa0fc8b22e34f2eff3b2858b0d15fd6725
André Gonçalves <andreslb1@gmail.com>
2014-04-01 22:37:15 +0100

    Created validator function created checkbounds

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var map;
 
 var app = {
 	tiles: "images/tiles",
-	blank_path: 'images/tiles' + '/blank.png',
+	temp_tiles: "images/new",
 	map: null,
 	markers: [],
 
@@ -10,24 +10,22 @@ var app = {
 		var self = this;
 		var deatlassenOptions = {
 		  getTileUrl: function(coord, zoom) {
-		    if (coord.x > -1 && coord.y > -1) {
-		      var path = self.tiles + '/' + (zoom) + '/tile-' + coord.x + '-' + coord.y + '.png';
-		      return path;
-		    } else {
-		      return self.blank_path;
-		    }
+		  	if (!self.validateLimit(coord,zoom)) {
+		  		return null
+		  	}
+		   	return self.temp_tiles + '/' + (zoom) + '/tile-' + coord.x + '-' + coord.y + '.png';
 		  },
 		  tileSize: new google.maps.Size(256, 256),
 		  maxZoom: 5,
 		  minZoom: 3,
-		  name: 'Map'
+		  name: 'De-Atlassen'
 		};
 
 		var deatlassenMapType = new google.maps.ImageMapType(deatlassenOptions);
 
-		var myLatlng = new google.maps.LatLng(0, 0);
+		var center = new google.maps.LatLng(65.80277639340224, -0.703125);
   		var mapOptions = {
-    		center: myLatlng,
+    		center: center,
     		zoom: 3,
 		    streetViewControl: false,
 		    mapTypeControlOptions: {
@@ -38,6 +36,16 @@ var app = {
   		self.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
   		self.map.mapTypes.set('deatlassen', deatlassenMapType);
   		self.map.setMapTypeId('deatlassen');
+	},
+
+	/* Map Bounds handler */
+	validateLimit: function(coord, zoom) {
+		var limit = Math.pow(2, zoom) - 1;
+		if (coord.x > -1 && coord.x <= (limit) && coord.y > -1 && coord.y <= (limit)) {
+			return true
+		} else {
+			return false
+		}
 	},
 
 	/* Markers handling functions */
@@ -88,7 +96,7 @@ var app = {
 
 $(document).ready(function() {
 	$(document).foundation();
-
+	
 	if (typeof(google) != 'undefined') {
 		app.initMap();
 		//app.getMarkers();
