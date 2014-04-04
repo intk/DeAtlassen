d1b03a87b31d13e0df55f173a769ca566687e9cd
André Gonçalves <andreslb1@gmail.com>
2014-04-04 14:30:39 +0100

    Start creating the front end page. Added side bar. Close button and div for the museum logo. Responsive tested.

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ var map;
 var app = {
 	tiles: "images/tiles",
 	temp_tiles: "images/new",
+	content_image: "#content-image",
+	close_description: "#close-description",
 	map: null,
 	markers: [],
 
@@ -23,10 +25,14 @@ var app = {
 
 		var deatlassenMapType = new google.maps.ImageMapType(deatlassenOptions);
 
-		var center = new google.maps.LatLng(63.626744644753124, -1.40625);
+		var center = new google.maps.LatLng(63.78248603116466, 4.04296875);
   		var mapOptions = {
     		center: center,
     		zoom: 3,
+    		zoomControl: true,
+    		zoomControlOptions: {
+    			style: google.maps.ZoomControlStyle.LARGE
+    		},
 		    streetViewControl: false,
 		    mapTypeControl: false,
 		    mapTypeControlOptions: {
@@ -50,15 +56,6 @@ var app = {
 	},
 
 	/* Markers handling functions */
-
-	addMarker: function(position) {
-		var marker = new google.maps.Marker({
-  			position: position,
-  			map: self.map,
-  			visible: true
-  		});
-	},
-
 	getMarkers: function(show) {
 		$.getJSON("static/markers.json", function(data) {
 			console.log(data);
@@ -76,6 +73,17 @@ var app = {
 	},
 
 	/* Markers helper functions */
+	addMarker: function(lat, lng) {
+		var position = new google.maps.LatLng(lat, lng);
+
+		var marker = new google.maps.Marker({
+			position: position,
+			title: 'Marker #1'
+		});
+
+		this.markers.push(marker);
+	},
+
 	showMarkers: function() {
 		for (var i = 0; i < this.markers.length; i++) {
 			this.markers[i].setMap(this.map);
@@ -91,16 +99,46 @@ var app = {
 	refreshMarkers: function(show) {
 		this.deleteMarkers();
 		this.getMarkers(show);
+	},
+
+	/* EVENTS */
+	triggerEvents: function() {
+		$(this.content_image).on("load", this.loadImageEvent);
+		$(this.close_description).click(this.closeDescription);
+	},
+
+	loadImageEvent: function() {
+		$("#map-canvas").hide();
+		$("#image-wrapper").show();
+	},
+
+	closeDescription: function() {
+		$("#description").fadeOut();
 	}
 }
 
-
 $(document).ready(function() {
+	// INIT Front-end framework
 	$(document).foundation();
-
+	
 	if (typeof(google) != 'undefined') {
+		// INIT Google Maps features
 		app.initMap();
+
+		// Trigger events
+		app.triggerEvents();
+
 		//app.getMarkers(false);
+		// Fake markers
+		app.addMarker(41.64007838467894, -52.91015625);
+		app.addMarker(41.11246878918085, -74.35546875);
+		app.addMarker(31.80289258670676, 37.6171875);
+		app.addMarker(41.508577297439324, 75.76171875);
+		app.addMarker(8.407168163601074, 19.86328125);
+		app.addMarker(57.136239319177434, -14.0625)
+		// Show markers
+		app.showMarkers();
+		
 	} else {
 		$("#map-canvas").html("Network disconnected.");
 	}
