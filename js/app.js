017accb3c02b7827a60873be3a963bff6831f5f4
André Gonçalves <andreslb1@gmail.com>
2014-04-15 17:18:04 +0100

    fix lgs

--- js/app.js
+++ js/app.js
@@ -152,13 +152,21 @@ var app = {
 		google.maps.event.addListener(app.map, 'click', function() {
 			app.hideAllMedia();
 		});
-		
+
 		$("#div1").click(function() {
 			app.hideAllMedia();
 		});
 		$(".poem").click(function() {
 			app.hideAllMedia();
 		});
+	},
+
+	sharer: function(url) {
+		var winWidth = 520;
+		var winHeight = 350;
+		var winTop = (screen.height / 2) - (winHeight / 2);
+        var winLeft = (screen.width / 2) - (winWidth / 2);
+        window.open(url, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width='+winWidth+',height='+winHeight);
 	},
 
 	/* CONTEXT Handler */
